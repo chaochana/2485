988db85860bb687c13f299afb408ded92f13798e
Chaochana.S <root@thkrn-data-1.c.thaikrn-269709.internal>
2020-03-28 13:01:53 +0000

    add https certbot

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home text-center">
+    <attendanceCounter/>
+  </div>
+</template>
+
+<script>
+import attendanceCounter from '@/components/attendanceCounter.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    attendanceCounter
+  }
+}
+</script>
